226350b1751c6c98c95b5fc38a1222f9e8337852
phatdoan <doanminhphat0212@gmail.com>
2020-12-13 20:48:52 +0200

    Second commit

--- utils/Colors.js
+++
@@ -1,6 +0,0 @@
-export const Green = '#329159';
-export const Black = '#151515';
-export const LightGray = "#DEDEDE";
-export const Blue = "#5F8FDD";
-export const Orange = "#DD8B5F";
-export const HalfBlack = "#898989";
